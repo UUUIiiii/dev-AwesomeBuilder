d4cf829bc2925ebdeb66f1e6a564701ab1724d21
Sorokin Denis <uuuiiiii.v@gmail.com>
2024-04-14 23:32:15 +0300

    feat: create renderInformation function

--- dev/assets/js/floor.js
+++ dev/assets/js/floor.js
@@ -2,107 +2,161 @@ const flatArray = [{
     id: 0,
     house: '7',
     floor: '2',
-    flatNumber: '1',
+    flatNumber: 1,
     rooms: '3',
     square: '79,3',
     price: '1900$',
     priceTotal: '150670$',
-    status: 'sold'
+    status: 'продана'
 },
 {
     id: 1,
     house: '7',
     floor: '2',
-    flatNumber: '2',
+    flatNumber: 2,
     rooms: '2',
     square: '60,7',
     price: '700$',
     priceTotal: '42000$',
-    status: 'sold'
+    status: 'продана'
 },
 {
     id: 2,
     house: '7',
-    floor: '1',
-    flatNumber: '3',
+    floor: '2',
+    flatNumber: 3,
     rooms: '3',
     square: '60,7',
     price: '700$',
     priceTotal: '42000$',
-    status: 'free',
+    status: 'вільна',
 },
 {
     id: 3,
     house: '7',
     floor: '2',
-    flatNumber: '4',
+    flatNumber: 4,
     rooms: '3',
     square: '82',
     price: "700$",
-    priceTotal: '2500',
-    status:'action'
+    priceTotal: '25000$',
+    status:'акція'
 },
 {
     id: 4,
     house: '7',
     floor: '2',
-    flatNumber: '5',
+    flatNumber: 5,
     rooms: '3',
     square: '79.7',
     price: '700$',
     priceTotal: '55790$',
-    status: 'action'
+    status: 'акція'
 },
 {
     id: 5,
     house: '7',
     floor: '2',
-    flatNumber: '6',
+    flatNumber: 6,
     rooms: '1',
     square: '39.2',
     price: '700$',
     priceTotal: '27440$',
-    status: 'bookin'
+    status: 'заброньована'
 },
 {
     id: 6,
     house: '7',
     floor: '2',
-    flatNumber: '7',
+    flatNumber: 7,
     rooms: '1',
     square: '40.0',
     price: '700$',
     priceTotal: '29400$',
-    status: 'sold'
+    status: 'продана'
 },
 {
     id: 7,
     house: '7',
     floor: '2',
-    flatNumber: '8',
+    flatNumber: 8,
     rooms: '1',
     square: '39.2',
     price: '700$',
     priceTotal: '27440$',
-    status: 'free'
+    status: 'вільна'
 },
 {
     id: 8,
     house: '7',
     floor: '2',
-    flatNumber: '9',
+    flatNumber: 9,
     rooms: '3',
     square: '79.3',
     price: '900$',
     priceTotal: '55510$',
-    status: 'action',
+    status: 'акція',
 }]
 
-
+const initialValue = [
+    {
+        id: 0,
+        house: '7',
+        floor: '2',
+        flatNumber: 1,
+        rooms: '3',
+        square: '79,3',
+        price: '1900$',
+        priceTotal: '150670$',
+        status: 'продана'
+    }
+];
 
 function installFloor () {
     const flats = document.querySelectorAll('.flat');
-    const floorInfo = document.querySelector('.floor-options')
+    const flatInfo = document.querySelector('.floor-options')
+
+    const renderInformation = (array) => {
+        const flatInformation = array.map(item => {
+            return(`<div class="floor-options">
+            <div class="floor-option-item">
+                <div>Номер будинку:</div>
+                <div>${item.house}</div>
+            </div>
+            <div class="floor-option-item">
+                <div>Поверх:</div>
+                <div>${item.floor}</div>
+            </div>
+            <div class="floor-option-item">
+                <div>Номер квартири:</div>
+                <div>${item.flatNumber}</div>
+            </div>
+            <div class="floor-option-item">
+                <div>Кіл-ть кімнат:</div>
+                <div>${item.rooms}</div>
+            </div>
+            <div class="floor-option-item">
+                <div>Площа квартири:</div>
+                <div>${item.square}</div>
+            </div>
+            <div class="floor-option-item">
+                <div>Ціна за м²:</div>
+                <div>${item.price}</div>
+            </div>
+            <div class="floor-option-item">
+                <div>Ціна за квартиру:</div>
+                <div>${item.priceTotal}</div>
+            </div>
+            <div class="floor-option-item">
+                <div>Статус квартири:</div>
+                <div>${item.status}</div>
+            </div>
+        </div>`)
+        })
+        flatInfo.innerHTML = flatInformation;
+    }
+    renderInformation(initialValue);
+
 
 
     const removeActiveClass = () => {
@@ -116,7 +170,9 @@ function installFloor () {
             flat.classList.add('active');
 
             const thisFlat = flat.getAttribute('data-flat-number');
-            console.log(thisFlat);
+
+            let flatNumber = flatArray.filter(item => item.flatNumber === Number(thisFlat));
+            renderInformation(flatNumber);
         })
     })
  }
